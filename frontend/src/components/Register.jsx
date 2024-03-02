a5ceff105e751744a776ad2eef93436ab6a01a8a
Prasoon321 <116250058+Prasoon321@users.noreply.github.com>
2024-03-02 16:41:15 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -6,7 +6,7 @@ function Register() {
   const userdsatasubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000", {
+      const response = await fetch("https://workingout.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json", // Fix the content-type header
